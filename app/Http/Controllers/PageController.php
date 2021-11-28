de8f02e1de6a8e100c13b5424db2437aa5573a2e
Omar Chouman <omar.chouman0@gmail.com>
2021-11-28 15:35:15 +0200

    Finished App and Page Controllers

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Page;
 
 class PageController extends Controller
 {
-    //
+    public function show($slug)
+    {
+        $page = Page::where('slug', $slug)->get();
+        if ($page->first()) {
+            return response()->json(['data' => $page]);
+        }
+        return response()->json(['message' => 'Couln\'nt find page you requested'],404);
+    }
+
 }
