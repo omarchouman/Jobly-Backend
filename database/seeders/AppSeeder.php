5c4c2cb86e17d1ab91e62c30cad031cf65abf95a
omarchouman <omar.chouman0@gmail.com>
2021-11-30 11:19:59 +0200

    Added app and category seeders

---
+++ database/seeders/AppSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\App;
+use Illuminate\Database\Seeder;
+
+class AppSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App::create([
+            
+        	'app_name' => 'Jobly',
+        	'app_slogan' => 'get your dream Job',
+        	'app_icon' => '',
+        	'app_logo' => '',
+        	'address' => 'Beirut, Lebanon',
+        	'phone' => '+961 78890304',
+        	'mobile' => ' +961 78890304',
+        	'email' => 'omar.chouman0@gmail.com',
+        	'facebook' => 'https://www.facebook.com/omar.chouman.58/',
+        	'github' => 'https://github.com/omarchouman',
+        	'youtube' => 'https://www.youtube.com/channel/UCuGFjOa6NMMzwq7YbZ9Z0Kg',
+        	'copyright_text' => 'Copyright © 2021 All rights reserved',
+        ]);
+    }
+}
